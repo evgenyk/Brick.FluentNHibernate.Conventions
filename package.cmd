120c3e46defd2909f04a492e140d38beb26cbffc
Evgeny Komarevtsev <evgeny@komarevtsev.net>
2015-08-10 23:33:49 +1000

    nugetted

---
+++ package.cmd
@@ -0,0 +1,19 @@
+@ECHO OFF
+
+CALL BUILD
+
+SETLOCAL
+SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
+
+IF EXIST %CACHED_NUGET% goto copynuget
+echo Downloading latest version of NuGet.exe...
+IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
+
+:copynuget
+IF EXIST .nuget\nuget.exe goto pack
+md .nuget
+copy %CACHED_NUGET% .nuget\nuget.exe > nul
+
+
+:pack
