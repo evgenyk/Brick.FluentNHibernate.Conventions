66cbab4bfaa6957c8a2e277d5d57968f820591c0
Evgeny Komarevtsev <evgeny@komarevtsev.net>
2015-08-22 23:52:02 +1000

    fixed primary key generation in case of Guids

--- src/Brick.FluentNHibernate.Conventions/Conventions/PrimaryKeyGeneratorConvention.cs
+++ src/Brick.FluentNHibernate.Conventions/Conventions/PrimaryKeyGeneratorConvention.cs
@@ -9,7 +9,7 @@ namespace Brick.FluentNHibernate.Conventions.Conventions
         public void Apply(IIdentityInstance id)
         {
             if (id.Type == typeof (Guid)) id.GeneratedBy.GuidComb();
-            if (id.Type == typeof (string)) id.GeneratedBy.UuidHex("N");
+            else if (id.Type == typeof (string)) id.GeneratedBy.UuidHex("N");
             else if (id.Type == typeof (int)) id.GeneratedBy.Identity();
             else if (id.Type == typeof (long)) id.GeneratedBy.Identity();
             else id.GeneratedBy.Assigned();
