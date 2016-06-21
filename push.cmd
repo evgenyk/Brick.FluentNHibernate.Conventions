400bfdcede95766d43ae209a8ac0989b4f17689a
Evgeny Komarevtsev <evgeny@komarevtsev.net>
2016-06-21 15:30:52 +1000

    v.1.0.11

--- push.cmd
+++ push.cmd
@@ -1,4 +1,4 @@
 CD _GeneratedNuGetPackages
 
-for /f %%X IN ('dir /b *.nupkg') do ..\.nuget\NuGet.exe push "%%~fX"
+for /f %%X IN ('dir /b *.nupkg') do ..\.nuget\NuGet.exe push "%%~fX" -Source https://www.nuget.org/api/v2/package
 CD ..
